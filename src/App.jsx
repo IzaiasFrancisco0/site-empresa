36fd6620c1c469d5dd92ae033c50600a0f7cf4b3
Izaias Francisco <izaiasfrancisco2020@gmail.com>
2026-06-03 19:43:06 -0300

    fix project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Contents from "./components/contents";
+import Contents from "./components/Contents";
 import NavBar from "./components/NavBar";
 import Products from "./components/Products";
 import Hero from "./components/Hero";
